5ef45aca57531baa353035fbdea79b7f8e431399
Filip Ljubic <f.ljubic7@hotmail.com>
2020-09-29 01:18:19 +0200

    messing around with animations

--- lib/screens/quotes.dart
+++ lib/screens/quotes.dart
@@ -1,11 +1,64 @@
 import 'package:flutter/material.dart';
 
-class Quotes extends StatelessWidget {
+class Quotes extends StatefulWidget {
+  @override
+  _QuotesState createState() => _QuotesState();
+}
+
+class _QuotesState extends State<Quotes> with SingleTickerProviderStateMixin {
+  final List<String> quotes = ['Quote1', 'NOT A QUOTE 2'];
+  AnimationController _animationController;
+  Animation _animation;
+
+  @override
+  void initState() {
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 2000),
+    );
+    _animation = Tween(begin: 0.0, end: 1.0).animate(_animationController);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  int _index = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        child: Text("Quotes!"),
+    _animationController.forward();
+    return GestureDetector(
+      onTap: () => setState(() {
+        _animationController.repeat();
+        _index++;
+        if (_index == quotes.length) {
+          _index = 0;
+        }
+      }),
+      child: Center(
+        child: Container(
+          child: FadeTransition(
+            child: Dismissible(
+              key: ValueKey(quotes[_index]),
+              onDismissed: (direction) => setState(() {
+                _animationController.repeat();
+                _index++;
+                if (_index == quotes.length) {
+                  _index = 0;
+                }
+              }),
+              child: Text(
+                quotes[_index],
+                style: TextStyle(fontSize: 30.0),
+              ),
+            ),
+            opacity: _animation,
+          ),
+        ),
       ),
     );
   }

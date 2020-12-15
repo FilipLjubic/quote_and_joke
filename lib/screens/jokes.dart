6b414aa31d0dba7b200811dba8386a2f789832b5
Filip Ljubic <f.ljubic7@hotmail.com>
2020-12-15 15:56:43 +0100

    animated background of jokes

--- lib/screens/jokes.dart
+++ lib/screens/jokes.dart
@@ -1,27 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:quote_and_joke/utils/screen_size_config.dart';
+import 'dart:math' as math;
 
-class Jokes extends StatelessWidget {
+// add Nunito font
+
+class Jokes extends HookWidget {
   @override
   Widget build(BuildContext context) {
+    final animationController = useAnimationController(
+      duration: const Duration(seconds: 10),
+      initialValue: 0,
+      lowerBound: -1,
+      upperBound: 1,
+    )..repeat();
+
+    final animationController2 = useAnimationController(
+      duration: const Duration(seconds: 5),
+      initialValue: 0,
+      lowerBound: -1,
+      upperBound: 1,
+    )..repeat();
+
     return Stack(
       children: [
-        Container(
-          height: SizeConfig.screenHeight * 0.5,
-          width: SizeConfig.screenWidth,
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-                begin: Alignment.bottomLeft,
-                end: Alignment.topCenter,
-                colors: [
-                  Theme.of(context).primaryColor.withOpacity(0.8),
-                  Theme.of(context).accentColor.withOpacity(0.8),
-                ]),
+        AnimatedBuilder(
+          animation: animationController,
+          child: const WobblyContainer(
+            opacity: 1,
+            height: 0.6,
+          ),
+          builder: (_, child) => ClipPath(
+            clipper: DrawIdleClipper(animationController.value),
+            child: child,
+          ),
+        ),
+        AnimatedBuilder(
+          animation: animationController2,
+          child: const WobblyContainer(
+            opacity: 0.3,
+            height: 0.61,
+          ),
+          builder: (_, child) => ClipPath(
+            clipper: DrawIdleClipper(animationController2.value),
+            child: child,
           ),
         ),
         Positioned(
           left: SizeConfig.blockSizeHorizontal * 3,
-          top: SizeConfig.screenHeight * 0.3,
+          top: SizeConfig.screenHeight * 0.25,
           child: SizedBox(
             width: SizeConfig.screenWidth * 0.95,
             child: Text(
@@ -36,7 +63,7 @@ class Jokes extends StatelessWidget {
           bottom: SizeConfig.screenHeight * 0.25,
           left: SizeConfig.blockSizeHorizontal * 3,
           child: Text(
-            "ur mom lol",
+            "hehexd",
             style: TextStyle(
               fontSize: SizeConfig.blockSizeHorizontal * 7.5,
               color: Colors.black87,
@@ -45,5 +72,70 @@ class Jokes extends StatelessWidget {
         )
       ],
     );
+  }
+}
+
+class WobblyContainer extends StatelessWidget {
+  const WobblyContainer({
+    this.opacity,
+    this.height,
+  });
+
+  final double opacity;
+  final double height;
+
+  @override
+  Widget build(BuildContext context) {
+    return Opacity(
+      opacity: opacity,
+      child: Container(
+        height: SizeConfig.screenHeight * height,
+        width: SizeConfig.screenWidth,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.bottomLeft,
+              end: Alignment.topCenter,
+              colors: [
+                Theme.of(context).primaryColor.withOpacity(0.8),
+                Theme.of(context).accentColor.withOpacity(0.8),
+              ]),
+        ),
+      ),
+    );
+  }
+}
+
+class DrawIdleClipper extends CustomClipper<Path> {
+  DrawIdleClipper(this.value);
+  var value;
+
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+    path.lineTo(0, size.height * 0.85);
+
+    final ctrlPoint = Offset(
+      size.width * 0.2 + (size.width * 0.07) * math.cos(value * math.pi),
+      size.height + 10 * math.sin(value * math.pi),
+    );
+
+    path.quadraticBezierTo(
+        ctrlPoint.dx, ctrlPoint.dy, size.width * 0.5, size.height * 0.92);
+
+    final ctrlPoint2 = Offset(
+      size.width * 0.9 - (size.width * 0.05) * math.cos(value * math.pi),
+      size.height * 0.8 - 10 * math.sin(value * math.pi),
+    );
+
+    path.quadraticBezierTo(
+        ctrlPoint2.dx, ctrlPoint2.dy, size.width, size.height * 0.87);
+
+    path.lineTo(size.width, 0);
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
+    return true;
   }
 }
